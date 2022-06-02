1080bebcca148df9497be34d6f626f88364f73fb
ViolatedSine <101035636+ViolatedSine@users.noreply.github.com>
2022-06-02 18:37:13 -0500

    added google sheets api for storage

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import random
 import os
 import discord
 
-
 load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -17,66 +16,78 @@ Auth = None
 Auth = service_account.Credentials.from_service_account_file(AuthFile, scopes=Scope)
 SheetID = r'1kbEYXbXg1CEhjiLs2dsvYy2VKw87MIcBWw5L52VFfrU'
 
-
-
-API_Client = build('sheets','v4', credentials=Auth)
+API_Client = build('sheets', 'v4', credentials=Auth)
 PLClient = API_Client.spreadsheets()
-PlayerList = PLClient.values().get(spreadsheetId=SheetID,range="A1:E2").execute()
+SavedList = PLClient.values().get(spreadsheetId=SheetID, range="A1:f4").execute()
+PlayerList = []
+for i in SavedList['values']:
+    PlayerList.append(i)
 print(PlayerList)
-
+class Deck():
+    def NewDeck(self):  # defines new call function to create a new deck of cards.
+        deck = []  # creates variable "deck" as empty list
+        for y in range(0, 4):  # 0-3 sets card Suit
+            for x in range(0, 13):  # 0-14 sets card Number/Face, addition of 2 needed to avoid 0-1 in list
+                deck.append([x + 2, y])  # adds newly created "card" as its own list by appending to deck list
+        return deck
+    def Shuffle(self):
+        print("hello world")
+class Game():
+    print("hello world")
 
 class Player():
-    def __init__(self, name, cash=100):
-        self.name = str(name)
+    def __init__(self,name="defaultPlayer",wins=0,total=0,cash=100):
+        self.name = name
         self.hand = []
-        self.history = []
-        self.totalgames = 0
-        self.wins = 0
-        self.cash = cash
+        self.status = [wins, total, cash]
 
-    def Value(self):
-        self.value = 0
-        for card in self.hand:
-            self.value += card.Value()
-        return self.value
+    def New(self,name, cash=100,wins=0,total=0):
+        self.name = name
+        self.hand = []
+        self.status = [wins,total,cash]
+        return self
 
     def Deal(self, card):
-        print("Dealing " + self.name + " a card", end ="\r")
+        print("Dealing " + self.name + " a card", end="\r")
         self.hand.append(card)
         self.Value()
 
     def Hand(self):
-        return self.hand
+        response = self.name + " has a "
+        for i in self.hand:
+            response = response + i.info + " and a "
 
-    def ShowCards(self):
-        displayStr = self.name + ", NewCards! \n"
-        for card in self.Hand():
-            displayStr = displayStr + str(card.Info()) + "\n"
-
-        return "- \n"+(11 * "---")+"\n" + displayStr
-
+        return response[:-6] +'\n'
 
     def Clear(self):
         self.hand = []
 
     def Stats(self):
-        return self.history
+        return self.name + " has " + str(self.status[0]) + " wins in " + \
+               str(self.status[1]) + " games played. They have " + str(self.status[2]) + " VioCoins"
+    def Cash(self,Amount):
+        self.status[2] = int(self.status[2])+Amount
+        if Amount > 0:
+            return self.name + " gained " + str(Amount) +" VioCoins"
+        else:
+            return self.name + " lost " + str(Amount) + " VioCoins"
 
-    def SetHistory(self, wins, total, cash):
-        self.wins = wins
-        self.totalgames = total
-        self.cash = cash
-
-    def RecordHistory(self,result):
-        self.totalgames += 1
+    def Result(self, result):
+        self.status[1] += 1
         if result:
-            self.wins += 1
-            self.cash +=50
+            self.status[0] = self.status[0]+1
+            self.status[2] = self.status[2]+50
         else:
             if self.name == "Dealer":
-                self.cash += -50
-        self.history = [self.wins, self.totalgames, self.cash]
-        return self.history
+                self.status[2] += -50
+        return self.status
+    def Value(self):
+        value = 0
+        for i in self.hand:
+            value = value + i.Value()[0]
+        return value
+    def Save(self):
+        print("hello world")
 
 class Card():
     suitList = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -126,6 +137,10 @@ class Card():
             self.number = 11
             self.info = str(self.faceList[value - 9]) + " of " + str(self.suitList[suit])
 
+    def Flip(self,value):
+        print(self.flipped)
+        self.flipped = value
+
     def Info(self):
         if self.flipped:
             return self.info
@@ -133,7 +148,7 @@ class Card():
             return "Hidden Card"
 
     def Value(self):
-        return self.number
+        return [self.number, self.suit]
 
     def Draw(self):
         if not self.flipped:
@@ -141,14 +156,15 @@ class Card():
         else:
             return self.image
 
+
 def PlayRound(player, dealer):
     dealer.Deal(Card(randint(0, 12), randint(0, 1), True))
     dealer.Deal(Card(randint(0, 12), randint(0, 1), False))
     player.Deal(Card(randint(0, 12), randint(0, 1), True))
     player.Deal(Card(randint(0, 12), randint(0, 1), True))
 
-    print(player.ShowCards())
-    print(dealer.ShowCards())
+    print(player.hand)
+    print(dealer.hand)
     while player.Value() < 22:
         action = input("Hit or Stay?")
         if action == "Hit":
@@ -175,22 +191,24 @@ def PlayRound(player, dealer):
     dealer.Clear()
     return input("Continue? t/f")
 
-dealer = Player("Dealer")
+
+dealer = Player(name=PlayerList[1][1],wins=PlayerList[1][2],total=PlayerList[1][3],cash=PlayerList[1][4])
 player = Player("Player")
 
-
 client = discord.Client()
+
+
 @client.event
 async def on_ready():
-     response = ["Casinos Now Open!","Come on in, everyone!"]
-     print(random.choice(response))
-     channel = client.get_channel(981247464826896424)
-     await channel.send('Casinos Open!')
+    response = ["Casinos Now Open!", "Come on in, everyone!"]
+    print(random.choice(response))
+    channel = client.get_channel(981247464826896424)
+    await channel.send('Casinos Open!')
+
 
 @client.event
 async def on_message(message):
     player.name = str(message.author)
-
 
     player.Value()
     dealer.Value()
@@ -206,50 +224,54 @@ async def on_message(message):
         response = dealer.Stats()
         await message.channel.send(response)
     if message.content == '!play':
-        player.cash += -10
-        dealer.cash += 10
+        player.Cash(-10)
+        dealer.Cash(10)
         player.Clear()
         dealer.Clear()
         player.Deal(Card(randint(0, 12), randint(0, 1), True))
         player.Deal(Card(randint(0, 12), randint(0, 1), True))
         dealer.Deal(Card(randint(0, 12), randint(0, 1), True))
         dealer.Deal(Card(randint(0, 12), randint(0, 1), False))
-        response = player.ShowCards() + dealer.ShowCards()
+        response = player.Hand() + dealer.Hand()
         if player.Value() > 21:
-            response = response + " You Lose " + str(player.Value()) + " to " + str(dealer.Value())
-            print(player.RecordHistory(False),
-                  (str(player.name) + " Lost " + str(player.Value()) + " to " + str(dealer.Value())))
-            dealer.RecordHistory(True)
+            player.Clear()
+            dealer.Clear()
+            player.Deal(Card(randint(0, 12), randint(0, 1), True))
+            player.Deal(Card(randint(0, 12), randint(0, 1), True))
+            dealer.Deal(Card(randint(0, 12), randint(0, 1), True))
+            dealer.Deal(Card(randint(0, 12), randint(0, 1), False))
+            response = player.Hand() + dealer.Hand()
         await message.channel.send(response)
     if message.content == '!hit':
         player.Deal(Card(randint(0, 12), randint(0, 1), True))
-        response = player.ShowCards() + dealer.ShowCards()
+        response = player.Hand() + dealer.Hand()
         if player.Value() > 21:
             response = response + str(player.name) + " Lost " + str(player.Value()) + " to " + str(dealer.Value())
-            print(player.RecordHistory(False), (str(player.name) + " Lost " + str(player.Value()) + " to "
+            print(player.Result(False), (str(player.name) + " Lost " + str(player.Value()) + " to "
                                                 + str(dealer.Value())))
-            dealer.RecordHistory(True)
+            dealer.Result(True)
         await message.channel.send(response)
     if message.content == "!stay":
         for i in dealer.Hand():
-            i.flipped = True
+            i.Flip(True)
         while dealer.Value() < 17:
             dealer.Deal(Card(randint(0, 12), randint(0, 1), True))
 
         if player.Value() >= dealer.Value() and player.Value() <= 21 or dealer.Value() > 21:
-            response = player.ShowCards() + dealer.ShowCards() + ( "You Win " + str(player.Value()) + " to " +
-                                                                   str(dealer.Value()))
-            print(player.RecordHistory(True),(str(player.name) + " Won " + str(player.Value()) + " to " +
-                                              str(dealer.Value())))
-            dealer.RecordHistory(False)
-        else:
-            response = player.ShowCards() + dealer.ShowCards() + " You Lose " + str(player.Value()) + " to " + \
-                       str(dealer.Value())
-            print(player.RecordHistory(False),(str(player.name) + " Lost " + str(player.Value()) + " to " +
+            response = player.Hand() + dealer.Hand() + ("You Win " + str(player.Value()) + " to " +
+                                                                  str(dealer.Value()))
+            print(player.Result(True), (str(player.name) + " Won " + str(player.Value()) + " to " +
                                                str(dealer.Value())))
-            dealer.RecordHistory(True)
+            dealer.Result(False)
+        else:
+            response = player.Hand() + dealer.Hand() + " You Lose " + str(player.Value()) + " to " + \
+                       str(dealer.Value())
+            print(player.Result(False), (str(player.name) + " Lost " + str(player.Value()) + " to " +
+                                                str(dealer.Value())))
+            dealer.Result(True)
         player.Clear()
         dealer.Clear()
         await message.channel.send(response)
+
 
 client.run(TOKEN)
